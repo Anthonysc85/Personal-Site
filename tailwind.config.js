34aa907f7d3008f9bb3a08631598171a760dc720
Anthony Cowan <anthony.cowan@expandtheroom.com>
2022-09-16 18:04:09 -0500

    install tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}",
-'./index.html',
-'./portfolio.html'],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ['./{*.html, js}' 
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
